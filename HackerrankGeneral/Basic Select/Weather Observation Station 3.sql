32c06c924e1d5681e0115af100d0a79f35c1339e
Kənan Orucov <114289804+KananOrujov@users.noreply.github.com>
2022-09-26 18:58:37 +0400

    Update Weather Observation Station 3.sql

--- HackerrankGeneral/Basic Select/Weather Observation Station 3.sql
+++ HackerrankGeneral/Basic Select/Weather Observation Station 3.sql
@@ -1,5 +1,5 @@
 /*
-8)Query a list of CITY names from STATION for cities that have an even ID number. 
+Query a list of CITY names from STATION for cities that have an even ID number. 
 Print the results in any order, but exclude duplicates from the answer.
 */
 
