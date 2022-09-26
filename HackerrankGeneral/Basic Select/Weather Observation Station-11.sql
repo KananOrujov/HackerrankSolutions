2b99513f0e36d1cac0e600043aa483c316f10174
Kənan Orucov <114289804+KananOrujov@users.noreply.github.com>
2022-09-26 19:00:01 +0400

    Update Weather Observation Station-11.sql

--- HackerrankGeneral/Basic Select/Weather Observation Station-11.sql
+++ HackerrankGeneral/Basic Select/Weather Observation Station-11.sql
@@ -1,5 +1,5 @@
 /*
-16)Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
 */
 
 SELECT DISTINCT    
